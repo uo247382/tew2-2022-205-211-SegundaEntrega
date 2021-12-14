13cdfd4e55bd0f910bf4092383ee9cc290841a41
uo247382 <uo247382@DESKTOP-A9MB9D4.mshome.net>
2021-12-14 18:53:28 +0100

    Eliminar de cliente

--- tew-2022-205-2011-Cliente/bbdd/data/Manager.bat
+++
@@ -1,1 +0,0 @@
-java -classpath ..\..\..\..\lib\hsqldb.jar org.hsqldb.util.DatabaseManagerSwing -url jdbc:hsqldb:hsql://localhost/localDB -driver org.hsqldb.jdbcDriver
--- tew-2022-205-2011-Cliente/bbdd/hsqldbServer.bat
+++
@@ -1,2 +0,0 @@
-cd data
-call startup.bat
